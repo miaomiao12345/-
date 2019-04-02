fd621efef6969fd384e9070fae69c1c4d946fa91
miaomiao12345 <2579014375@qq.com>
2019-04-02 10:58:59 +0800

    2019/4/2

--- pages/wawy/wawy.js
+++ pages/wawy/wawy.js
@@ -1,4 +1,8 @@
+var util = require('../../utils/util.js');
 Page({
+  data:{
+    start_data:"2019/2/4"
+  },
   quweibo:function(e) {
     wx.navigateTo({
       url: '../weibo/weibo',
@@ -6,5 +10,16 @@ Page({
       fail:function() {},
       complete:function() {}
     })
+  },
+  onLoad:function() {
+    var time = util.formatTime(new Date());
+    var startTime = new Date(this.data.start_data);
+    var endTime = new Date(time);
+    var days = endTime.getTime()-startTime.getTime();
+    var day = parseInt(days/(1000*60*60*24));
+    this.setData({
+      time: time,
+      num:day
+    });
   }
 })
